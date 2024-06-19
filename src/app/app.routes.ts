f26a5d59ecb8891858bd305ccdf80ca5f4b622a8
Shubham Patel <ptlshubham@gmail.com>
2024-06-19 18:03:35 +0530

    Extra Component Remove

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -57,15 +57,6 @@ import { HdTicketDetailsComponent } from './pages/help-desk-page/hd-ticket-detai
 import { HdAgentsComponent } from './pages/help-desk-page/hd-agents/hd-agents.component';
 import { HdReportsComponent } from './pages/help-desk-page/hd-reports/hd-reports.component';
 import { EOrderTrackingComponent } from './pages/ecommerce-page/e-order-tracking/e-order-tracking.component';
-import { CalendarComponent } from './apps/calendar/calendar.component';
-import { ContactsComponent } from './apps/contacts/contacts.component';
-import { ToDoListComponent } from './apps/to-do-list/to-do-list.component';
-import { ChatComponent } from './apps/chat/chat.component';
-import { FileManagerComponent } from './apps/file-manager/file-manager.component';
-import { EmailComponent } from './apps/email/email.component';
-import { InboxComponent } from './apps/email/inbox/inbox.component';
-import { ComposeComponent } from './apps/email/compose/compose.component';
-import { ReadComponent } from './apps/email/read/read.component';
 import { EventsPageComponent } from './pages/events-page/events-page.component';
 import { EventsListComponent } from './pages/events-page/events-list/events-list.component';
 import { EventDetailsComponent } from './pages/events-page/event-details/event-details.component';
@@ -75,17 +66,8 @@ import { InvoicesPageComponent } from './pages/invoices-page/invoices-page.compo
 import { InvoicesComponent } from './pages/invoices-page/invoices/invoices.component';
 import { InvoiceDetailsComponent } from './pages/invoices-page/invoice-details/invoice-details.component';
 import { CContactsComponent } from './pages/crm-page/c-contacts/c-contacts.component';
-import { MyDriveComponent } from './apps/file-manager/my-drive/my-drive.component';
-import { AssetsComponent } from './apps/file-manager/assets/assets.component';
-import { ProjectsComponent } from './apps/file-manager/projects/projects.component';
-import { PersonalComponent } from './apps/file-manager/personal/personal.component';
-import { ApplicationsComponent } from './apps/file-manager/applications/applications.component';
-import { DocumentsComponent } from './apps/file-manager/documents/documents.component';
-import { MediaComponent } from './apps/file-manager/media/media.component';
-import { StarterComponent } from './starter/starter.component';
-import { PricingPageComponent } from './pages/pricing-page/pricing-page.component';
-import { KanbanBoardComponent } from './apps/kanban-board/kanban-board.component';
 import { SocialPageComponent } from './pages/social-page/social-page.component';
+import { PricingPageComponent } from './pages/pricing-page/pricing-page.component';
 import { ProfileComponent } from './pages/social-page/profile/profile.component';
 import { TimelineComponent } from './pages/social-page/profile/timeline/timeline.component';
 import { AboutComponent } from './pages/social-page/profile/about/about.component';
@@ -99,9 +81,6 @@ import { ProfilePageComponent } from './pages/profile-page/profile-page.componen
 import { UserProfileComponent } from './pages/profile-page/user-profile/user-profile.component';
 import { TeamsComponent } from './pages/profile-page/teams/teams.component';
 import { PProjectsComponent } from './pages/profile-page/p-projects/p-projects.component';
-import { IconsComponent } from './icons/icons.component';
-import { MaterialSymbolsComponent } from './icons/material-symbols/material-symbols.component';
-import { RemixiconComponent } from './icons/remixicon/remixicon.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { SignInComponent } from './authentication/sign-in/sign-in.component';
 import { SignUpComponent } from './authentication/sign-up/sign-up.component';
@@ -120,73 +99,13 @@ import { PrivacyPolicyComponent } from './settings/privacy-policy/privacy-policy
 import { TermsConditionsComponent } from './settings/terms-conditions/terms-conditions.component';
 import { MapsPageComponent } from './pages/maps-page/maps-page.component';
 import { InternalErrorComponent } from './common/internal-error/internal-error.component';
-import { BlankPageComponent } from './blank-page/blank-page.component';
 import { ComingSoonPageComponent } from './pages/coming-soon-page/coming-soon-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { GalleryPageComponent } from './pages/gallery-page/gallery-page.component';
 import { TestimonialsPageComponent } from './pages/testimonials-page/testimonials-page.component';
 import { TimelinePageComponent } from './pages/timeline-page/timeline-page.component';
-import { ChartsComponent } from './charts/charts.component';
-import { GaugeComponent } from './charts/gauge/gauge.component';
-import { ApexchartsComponent } from './charts/apexcharts/apexcharts.component';
-import { TablesComponent } from './tables/tables.component';
-import { BasicTableComponent } from './tables/basic-table/basic-table.component';
-import { DataTableComponent } from './tables/data-table/data-table.component';
-import { WidgetsComponent } from './widgets/widgets.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
-import { UiElementsComponent } from './ui-elements/ui-elements.component';
-import { AlertsComponent } from './ui-elements/alerts/alerts.component';
-import { AutocompleteComponent } from './ui-elements/autocomplete/autocomplete.component';
-import { InputComponent } from './ui-elements/input/input.component';
-import { AvatarsComponent } from './ui-elements/avatars/avatars.component';
-import { BadgesComponent } from './ui-elements/badges/badges.component';
-import { BreadcrumbComponent } from './ui-elements/breadcrumb/breadcrumb.component';
-import { ButtonToggleComponent } from './ui-elements/button-toggle/button-toggle.component';
-import { DatepickerComponent } from './ui-elements/datepicker/datepicker.component';
-import { AccordionComponent } from './ui-elements/accordion/accordion.component';
-import { BottomSheetComponent } from './ui-elements/bottom-sheet/bottom-sheet.component';
-import { ButtonsComponent } from './ui-elements/buttons/buttons.component';
-import { CardsComponent } from './ui-elements/cards/cards.component';
-import { CarouselsComponent } from './ui-elements/carousels/carousels.component';
-import { CheckboxComponent } from './ui-elements/checkbox/checkbox.component';
-import { ChipsComponent } from './ui-elements/chips/chips.component';
-import { ColorPickerComponent } from './ui-elements/color-picker/color-picker.component';
-import { DialogComponent } from './ui-elements/dialog/dialog.component';
-import { DividerComponent } from './ui-elements/divider/divider.component';
-import { GridListComponent } from './ui-elements/grid-list/grid-list.component';
-import { FormFieldComponent } from './ui-elements/form-field/form-field.component';
-import { DragDropComponent } from './ui-elements/drag-drop/drag-drop.component';
-import { ExpansionComponent } from './ui-elements/expansion/expansion.component';
-import { ClipboardComponent } from './ui-elements/clipboard/clipboard.component';
-import { IconComponent } from './ui-elements/icon/icon.component';
-import { ListComponent } from './ui-elements/list/list.component';
-import { ListboxComponent } from './ui-elements/listbox/listbox.component';
-import { MenusComponent } from './ui-elements/menus/menus.component';
-import { PaginationComponent } from './ui-elements/pagination/pagination.component';
-import { ProgressBarComponent } from './ui-elements/progress-bar/progress-bar.component';
-import { RadioComponent } from './ui-elements/radio/radio.component';
-import { SelectComponent } from './ui-elements/select/select.component';
-import { SidenavComponent } from './ui-elements/sidenav/sidenav.component';
-import { SlideToggleComponent } from './ui-elements/slide-toggle/slide-toggle.component';
-import { SliderComponent } from './ui-elements/slider/slider.component';
-import { SnackbarComponent } from './ui-elements/snackbar/snackbar.component';
-import { StepperComponent } from './ui-elements/stepper/stepper.component';
-import { TypographyComponent } from './ui-elements/typography/typography.component';
-import { TooltipComponent } from './ui-elements/tooltip/tooltip.component';
-import { ToolbarComponent } from './ui-elements/toolbar/toolbar.component';
-import { VideosComponent } from './ui-elements/videos/videos.component';
-import { TreeComponent } from './ui-elements/tree/tree.component';
-import { TabsComponent } from './ui-elements/tabs/tabs.component';
-import { TableComponent } from './ui-elements/table/table.component';
-import { FormsComponent } from './forms/forms.component';
-import { BasicElementsComponent } from './forms/basic-elements/basic-elements.component';
-import { AdvancedElementsComponent } from './forms/advanced-elements/advanced-elements.component';
-import { WizardComponent } from './forms/wizard/wizard.component';
-import { EditorsComponent } from './forms/editors/editors.component';
-import { FileUploaderComponent } from './forms/file-uploader/file-uploader.component';
 import { FaqPageComponent } from './pages/faq-page/faq-page.component';
-import { RatioComponent } from './ui-elements/ratio/ratio.component';
-import { UtilitiesComponent } from './ui-elements/utilities/utilities.component';
 
 export const routes: Routes = [
     {path: '', component: EcommerceComponent},
@@ -194,33 +113,6 @@ export const routes: Routes = [
     {path: 'project-management', component: ProjectManagementComponent},
     {path: 'lms', component: LmsComponent},
     {path: 'help-desk', component: HelpDeskComponent},
-    {path: 'to-do-list', component: ToDoListComponent},
-    {path: 'calendar', component: CalendarComponent},
-    {path: 'contacts', component: ContactsComponent},
-    {path: 'chat', component: ChatComponent},
-    {path: 'kanban-board', component: KanbanBoardComponent},
-    {
-        path: 'file-manager',
-        component: FileManagerComponent,
-        children: [
-            {path: '', component: MyDriveComponent},
-            {path: 'assets', component: AssetsComponent},
-            {path: 'projects', component: ProjectsComponent},
-            {path: 'personal', component: PersonalComponent},
-            {path: 'applications', component: ApplicationsComponent},
-            {path: 'documents', component: DocumentsComponent},
-            {path: 'media', component: MediaComponent}
-        ]
-    },
-    {
-        path: 'email',
-        component: EmailComponent,
-        children: [
-            {path: '', component: InboxComponent},
-            {path: 'compose', component: ComposeComponent},
-            {path: 'read', component: ReadComponent}
-        ]
-    },
     {
         path: 'ecommerce-page',
         component: EcommercePageComponent,
@@ -333,7 +225,6 @@ export const routes: Routes = [
             {path: 'settings', component: ProfileSettingsComponent}
         ]
     },
-    {path: 'starter', component: StarterComponent},
     {path: 'faq', component: FaqPageComponent},
     {path: 'pricing', component: PricingPageComponent},
     {path: 'maps', component: MapsPageComponent},
@@ -355,14 +246,6 @@ export const routes: Routes = [
             {path: '', component: UserProfileComponent},
             {path: 'teams', component: TeamsComponent},
             {path: 'projects', component: PProjectsComponent},
-        ]
-    },
-    {
-        path: 'icons',
-        component: IconsComponent,
-        children: [
-            {path: '', component: MaterialSymbolsComponent},
-            {path: 'remixicon', component: RemixiconComponent}
         ]
     },
     {
@@ -395,87 +278,7 @@ export const routes: Routes = [
     {path: 'testimonials', component: TestimonialsPageComponent},
     {path: 'search', component: SearchPageComponent},
     {path: 'coming-soon', component: ComingSoonPageComponent},
-    {path: 'blank-page', component: BlankPageComponent},
     {path: 'internal-error', component: InternalErrorComponent},
-    {path: 'widgets', component: WidgetsComponent},
-    {
-        path: 'charts',
-        component: ChartsComponent,
-        children: [
-            {path: '', component: ApexchartsComponent},
-            {path: 'gauge', component: GaugeComponent},
-        ]
-    },
-    {
-        path: 'tables',
-        component: TablesComponent,
-        children: [
-            {path: '', component: BasicTableComponent},
-            {path: 'data-table', component: DataTableComponent},
-        ]
-    },
-    {
-        path: 'ui-kit',
-        component: UiElementsComponent,
-        children: [
-            {path: '', component: AlertsComponent},
-            {path: 'autocomplete', component: AutocompleteComponent},
-            {path: 'avatars', component: AvatarsComponent},
-            {path: 'accordion', component: AccordionComponent},
-            {path: 'badges', component: BadgesComponent},
-            {path: 'breadcrumb', component: BreadcrumbComponent},
-            {path: 'button-toggle', component: ButtonToggleComponent},
-            {path: 'bottom-sheet', component: BottomSheetComponent},
-            {path: 'buttons', component: ButtonsComponent},
-            {path: 'cards', component: CardsComponent},
-            {path: 'carousels', component: CarouselsComponent},
-            {path: 'checkbox', component: CheckboxComponent},
-            {path: 'chips', component: ChipsComponent},
-            {path: 'color-picker', component: ColorPickerComponent},
-            {path: 'clipboard', component: ClipboardComponent},
-            {path: 'datepicker', component: DatepickerComponent},
-            {path: 'dialog', component: DialogComponent},
-            {path: 'divider', component: DividerComponent},
-            {path: 'drag-drop', component: DragDropComponent},
-            {path: 'expansion', component: ExpansionComponent},
-            {path: 'form-field', component: FormFieldComponent},
-            {path: 'grid-list', component: GridListComponent},
-            {path: 'input', component: InputComponent},
-            {path: 'icon', component: IconComponent},
-            {path: 'list', component: ListComponent},
-            {path: 'listbox', component: ListboxComponent},
-            {path: 'menus', component: MenusComponent},
-            {path: 'pagination', component: PaginationComponent},
-            {path: 'progress-bar', component: ProgressBarComponent},
-            {path: 'radio', component: RadioComponent},
-            {path: 'ratio', component: RatioComponent},
-            {path: 'select', component: SelectComponent},
-            {path: 'sidenav', component: SidenavComponent},
-            {path: 'slide-toggle', component: SlideToggleComponent},
-            {path: 'slider', component: SliderComponent},
-            {path: 'snackbar', component: SnackbarComponent},
-            {path: 'stepper', component: StepperComponent},
-            {path: 'typography', component: TypographyComponent},
-            {path: 'tooltip', component: TooltipComponent},
-            {path: 'toolbar', component: ToolbarComponent},
-            {path: 'table', component: TableComponent},
-            {path: 'tabs', component: TabsComponent},
-            {path: 'tree', component: TreeComponent},
-            {path: 'videos', component: VideosComponent},
-            {path: 'utilities', component: UtilitiesComponent},
-        ]
-    },
-    {
-        path: 'forms',
-        component: FormsComponent,
-        children: [
-            {path: '', component: BasicElementsComponent},
-            {path: 'advanced-elements', component: AdvancedElementsComponent},
-            {path: 'wizard', component: WizardComponent},
-            {path: 'editors', component: EditorsComponent},
-            {path: 'file-uploader', component: FileUploaderComponent},
-        ]
-    },
     // Here add new pages component
 
     {path: '**', component: NotFoundComponent} // This line will remain down from the whole pages component list
